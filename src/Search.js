8ee3bb612b3c07cd2d8ed6d2901e6085802bc266
Douglas <douglaskerins@gmail.com>
2021-11-30 12:27:20 +0000

    adding styling to search route

--- src/Search.js
+++ src/Search.js
@@ -6,13 +6,37 @@ function Search() {
     <div className="Search">
       <div className="planContainer">
         <div className="mealBox">
-          <h1>Meal 1</h1>
+          {/* <h1>Meal 1</h1> */}
+          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
         </div>
         <div className="mealBox">
         <h1>Meal 2</h1>
+          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
         </div>
         <div className="mealBox">
         <h1>Meal 3</h1>
+          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
+        </div>
+        <div className="macrosContainer">
+          <h1>Plan Macros:</h1>
+          <h3>Fats:</h3>
+          <h3>Carbs:</h3>
+          <h3>Protein:</h3>
+          <h3>Calories:</h3>
+        </div>
+      </div>
+      <div className="planContainer">
+        <div className="mealBox">
+          <h1>Meal 1</h1>
+          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
+        </div>
+        <div className="mealBox">
+        <h1>Meal 2</h1>
+          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
+        </div>
+        <div className="mealBox">
+        <h1>Meal 3</h1>
+          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
         </div>
         <div className="macrosContainer">
           <h1>Plan Macros:</h1>
