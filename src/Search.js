41bc38a076628ff09c13c3630ffdb8b9fa7d11dd
Douglas <douglaskerins@gmail.com>
2021-11-30 13:46:04 +0000

    further styling search page

--- src/Search.js
+++ src/Search.js
@@ -5,27 +5,7 @@ function Search() {
   return (
     <div className="Search">
       <div className="planContainer">
-        <div className="mealBox">
-          {/* <h1>Meal 1</h1> */}
-          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
-        </div>
-        <div className="mealBox">
-        <h1>Meal 2</h1>
-          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
-        </div>
-        <div className="mealBox">
-        <h1>Meal 3</h1>
-          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
-        </div>
-        <div className="macrosContainer">
-          <h1>Plan Macros:</h1>
-          <h3>Fats:</h3>
-          <h3>Carbs:</h3>
-          <h3>Protein:</h3>
-          <h3>Calories:</h3>
-        </div>
-      </div>
-      <div className="planContainer">
+        <h1>Plan Name</h1>
         <div className="mealBox">
           <h1>Meal 1</h1>
           <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
@@ -39,11 +19,10 @@ function Search() {
           <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
         </div>
         <div className="macrosContainer">
-          <h1>Plan Macros:</h1>
-          <h3>Fats:</h3>
-          <h3>Carbs:</h3>
-          <h3>Protein:</h3>
-          <h3>Calories:</h3>
+          <span>Fats: <strong>45</strong></span>
+          <span>Carbs: <strong>345</strong></span>
+          <span>Protein: <strong>60</strong></span>
+          <span>Calories: <strong>100</strong></span>
         </div>
       </div>
     </div>
