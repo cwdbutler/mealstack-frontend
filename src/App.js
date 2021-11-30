584b92a1c3b5b5fe0d0e53d4f058ae0fa30dd699
ConorButler <cwdbutler@gmail.com>
2021-11-30 11:06:52 +0000

    Home page with a form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/home" exact element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/Home.js
@@ -0,0 +1,39 @@
+import './Home.css';
+
+function Home() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('You have submitted the form.');
+  };
+
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <h1>Form</h1>
+      </header>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Protein</p>
+            <input name="protein" />
+          </label>
+          <label>
+            <p>Fats</p>
+            <input name="fats" />
+          </label>
+          <label>
+            <p>Carbs</p>
+            <input name="carbs" />
+          </label>
+          <label>
+            <p>Calories</p>
+            <input name="calories" />
+          </label>
+        </fieldset>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
+
+export default Home;
