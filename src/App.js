c2eaeb8ddb69339818403a1e6ef97ebf273053ab
ConorButler <cwdbutler@gmail.com>
2021-11-30 15:38:08 +0000

    Basic navbar for existing routes

--- src/App.js
+++ src/App.js
@@ -2,28 +2,41 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import PlanInfo from './PlanInfo';
 import Search from './Search';
+import './App.css';
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/plan">PlanInfo</Link>
-              <Link to="/search">Search</Link>
-            </li>
-          </ul>
+      <>
+        <nav className="navbar">
+          <span>
+            <Link to="/home">
+              <button className="navbar-btn" type="button">
+                Home
+              </button>
+            </Link>
+          </span>
+          <span>
+            <Link to="/search">
+              <button className="navbar-btn" type="button">
+                Search
+              </button>
+            </Link>
+          </span>
+          <span>
+            <Link to="/plan">
+              <button className="navbar-btn" type="button">
+                Plan
+              </button>
+            </Link>
+          </span>
         </nav>
         <Routes>
           <Route path="/home" exact element={<Home />} />
           <Route path="/plan" exact element={<PlanInfo />} />
           <Route path="/search" exact element={<Search />} />
         </Routes>
-      </div>
+      </>
     </Router>
   );
 }
