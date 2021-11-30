5a11416f060e20281b28d028d5868c22fa022921
Douglas <douglaskerins@gmail.com>
2021-11-30 14:16:11 +0000

    extracting plan to separate component

---
+++ src/Plan.js
@@ -0,0 +1,25 @@
+export default function Plan() {
+  return (
+    <div className="planContainer">
+    <h1>Plan Name</h1>
+    <div className="mealBox">
+      <h1>Meal 1</h1>
+      <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
+    </div>
+    <div className="mealBox">
+    <h1>Meal 2</h1>
+      <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
+    </div>
+    <div className="mealBox">
+    <h1>Meal 3</h1>
+      <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
+    </div>
+    <div className="macrosContainer">
+      <span>Fats: <strong>45</strong></span>
+      <span>Carbs: <strong>345</strong></span>
+      <span>Protein: <strong>60</strong></span>
+      <span>Calories: <strong>100</strong></span>
+    </div>
+  </div>
+  );
+}
--- src/Search.js
+++ src/Search.js
@@ -1,30 +1,10 @@
 import './Search.css';
+import Plan from './Plan';
 
 function Search() {
-
   return (
     <div className="Search">
-      <div className="planContainer">
-        <h1>Plan Name</h1>
-        <div className="mealBox">
-          <h1>Meal 1</h1>
-          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
-        </div>
-        <div className="mealBox">
-        <h1>Meal 2</h1>
-          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
-        </div>
-        <div className="mealBox">
-        <h1>Meal 3</h1>
-          <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" alt="" />
-        </div>
-        <div className="macrosContainer">
-          <span>Fats: <strong>45</strong></span>
-          <span>Carbs: <strong>345</strong></span>
-          <span>Protein: <strong>60</strong></span>
-          <span>Calories: <strong>100</strong></span>
-        </div>
-      </div>
+      <Plan />
     </div>
   );
 }
