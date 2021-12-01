aaa1c1f33da6fc8f6c94f047f34c2eea123c6348
ConorButler <cwdbutler@gmail.com>
2021-12-01 16:24:08 +0000

    Basic shopping list style

--- src/PlanInfo.js
+++ src/PlanInfo.js
@@ -1,4 +1,15 @@
-import { Grid, GridItem, Heading } from '@chakra-ui/react';
+import {
+  Grid,
+  GridItem,
+  Heading,
+  Box,
+  Flex,
+  Spacer,
+  Image,
+  ListItem,
+  UnorderedList,
+  Stack,
+} from '@chakra-ui/react';
 import { Center } from '@chakra-ui/layout';
 
 export default function PlanInfo() {
@@ -14,9 +25,61 @@ export default function PlanInfo() {
         templateColumns="repeat(6, 1fr)"
         gap={4}
       >
-        <GridItem rowSpan={3} colSpan={4} bg="papayawhip"></GridItem>
-        <GridItem rowSpan={10} colSpan={2} bg="tomato">
-          Shopping List
+        <GridItem rowSpan={3} colSpan={4} bg="papayawhip">
+          <Flex h="100%" p="5">
+            <Image
+              src="https://insanelygoodrecipes.com/wp-content/uploads/2020/12/Chocolate-Chip-Pancakes.png"
+              alt="Breakfast"
+              borderRadius="10"
+            />
+            <Spacer />
+            <Box bg="yellow" w="60%" borderRadius="10">
+              Lorem ipsum dolor sit amet adipiscing ipsum dolor sit amet
+              adipiscing ipsum dolor sit amet adipiscing ipsum dolor sit amet
+              adipiscing ipsum dolor sit amet adipiscing
+            </Box>
+          </Flex>
+        </GridItem>
+        <GridItem rowSpan={10} colSpan={2}>
+          <Flex
+            d="column"
+            h="100%"
+            p="5"
+            overflow="scroll"
+            border="2px"
+            borderColor="gray.200"
+            borderRadius="10"
+          >
+            <Heading>Shopping List</Heading>
+            <UnorderedList mt="5">
+              <Stack spacing={4}>
+                <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                <ListItem>Consectetur adipiscing elit</ListItem>
+                <ListItem>Integer molestie lorem at massa</ListItem>
+                <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+                <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                <ListItem>Consectetur adipiscing elit</ListItem>
+                <ListItem>Integer molestie lorem at massa</ListItem>
+                <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+                <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                <ListItem>Consectetur adipiscing elit</ListItem>
+                <ListItem>Integer molestie lorem at massa</ListItem>
+                <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+                <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                <ListItem>Consectetur adipiscing elit</ListItem>
+                <ListItem>Integer molestie lorem at massa</ListItem>
+                <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+                <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                <ListItem>Consectetur adipiscing elit</ListItem>
+                <ListItem>Integer molestie lorem at massa</ListItem>
+                <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+                <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                <ListItem>Consectetur adipiscing elit</ListItem>
+                <ListItem>Integer molestie lorem at massa</ListItem>
+                <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+              </Stack>
+            </UnorderedList>
+          </Flex>
         </GridItem>
         <GridItem rowSpan={3} colSpan={4} bg="papayawhip">
           Lunch
