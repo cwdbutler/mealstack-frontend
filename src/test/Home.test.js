649d1ff24372e07170fe803ea9722ce63d4bcda6
Douglas <douglaskerins@gmail.com>
2021-12-03 08:53:38 +0000

    moving test folder

--- src/test/Home.test.js
+++ src/test/Home.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import App from '../src/App';
-import Home from '../src/App';
+import App from '../App';
+import Home from '../App';
 
 test('renders app name', () => {
   render(<Home />);
