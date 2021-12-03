ce29dddaf6b561dc5140de651db350c14eef652f
ConorButler <cwdbutler@gmail.com>
2021-12-03 21:23:11 +0000

    Reorganised home/searchform components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,18 @@
-import './SearchForm';
+import { Box, Center, HStack } from '@chakra-ui/layout';
+import { Heading } from '@chakra-ui/react';
 import SearchForm from './SearchForm';
 
 export default function Home() {
-  return <SearchForm />;
+  return (
+    <Center h="100%" w="100%">
+      <HStack spacing={20}>
+        <Heading as="h1" size="2xl">
+          Find your Mealstack...
+        </Heading>
+        <Box boxShadow="lg" p={10} borderRadius="lg">
+          <SearchForm />
+        </Box>
+      </HStack>
+    </Center>
+  );
 }
