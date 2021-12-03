c19c583d8ab7d91132705716e22de537b457d8c5
ConorButler <cwdbutler@gmail.com>
2021-12-03 18:27:02 +0000

    Form input working

--- src/components/Home/SearchForm.js
+++ src/components/Home/SearchForm.js
@@ -7,60 +7,132 @@ import {
   NumberIncrementStepper,
   NumberDecrementStepper,
 } from '@chakra-ui/react';
-import { FormControl, FormLabel, FormHelperText } from '@chakra-ui/react';
+import { FormControl, FormLabel } from '@chakra-ui/react';
+import { Formik, Form, Field } from 'formik';
 
 export default function SearchForm() {
   return (
     <Center h="90vh">
       <Box w="50vh">
-        <FormControl as="fieldset">
-          <Stack spacing={4}>
-            <FormControl id="protein">
-              <FormLabel>Protein</FormLabel>
-              <NumberInput step={10} defaultValue={100} min={0} max={300}>
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </FormControl>
-            <FormControl id="fats">
-              <FormLabel>Fats</FormLabel>
-              <NumberInput step={10} defaultValue={100} min={0} max={300}>
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </FormControl>
-            <FormControl id="carbs">
-              <FormLabel>Carbs</FormLabel>
-              <NumberInput step={10} defaultValue={100} min={0} max={300}>
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </FormControl>
-            <FormControl id="calories">
-              <FormLabel>Calories</FormLabel>
-              <NumberInput step={100} defaultValue={2000} min={500} max={8000}>
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </FormControl>
-            <Button>Search plans</Button>
-            <FormHelperText textAlign="center">
-              Find your Mealstack
-            </FormHelperText>
-          </Stack>
-        </FormControl>
+        <Formik
+          initialValues={{
+            protein: '80',
+            fat: '80',
+            carbs: '80',
+            calories: '2000',
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              // async fetch POST to server goes here
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 1000);
+          }}
+        >
+          {(props) => (
+            <Form>
+              <Stack spacing={4}>
+                <Field name="protein">
+                  {({ field, form }) => (
+                    <FormControl id="protein">
+                      <FormLabel htmlFor="protein">Protein</FormLabel>
+                      <NumberInput
+                        id="protein"
+                        {...field}
+                        onChange={(val) => form.setFieldValue(field.name, val)}
+                        step={10}
+                        defaultValue={80}
+                        min={0}
+                        max={300}
+                      >
+                        <NumberInputField />
+                        <NumberInputStepper>
+                          <NumberIncrementStepper />
+                          <NumberDecrementStepper />
+                        </NumberInputStepper>
+                      </NumberInput>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="fat">
+                  {({ field, form }) => (
+                    <FormControl id="fat">
+                      <FormLabel htmlFor="fat">Calories</FormLabel>
+                      <NumberInput
+                        id="fat"
+                        {...field}
+                        onChange={(val) => form.setFieldValue(field.name, val)}
+                        step={10}
+                        defaultValue={80}
+                        min={0}
+                        max={300}
+                      >
+                        <NumberInputField />
+                        <NumberInputStepper>
+                          <NumberIncrementStepper />
+                          <NumberDecrementStepper />
+                        </NumberInputStepper>
+                      </NumberInput>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="carbs">
+                  {({ field, form }) => (
+                    <FormControl id="carbs">
+                      <FormLabel htmlFor="carbs">Calories</FormLabel>
+                      <NumberInput
+                        id="carbs"
+                        {...field}
+                        onChange={(val) => form.setFieldValue(field.name, val)}
+                        step={10}
+                        defaultValue={80}
+                        min={0}
+                        max={300}
+                      >
+                        <NumberInputField />
+                        <NumberInputStepper>
+                          <NumberIncrementStepper />
+                          <NumberDecrementStepper />
+                        </NumberInputStepper>
+                      </NumberInput>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="calories">
+                  {({ field, form }) => (
+                    <FormControl id="calories">
+                      <FormLabel htmlFor="calories">Calories</FormLabel>
+                      <NumberInput
+                        id="calories"
+                        {...field}
+                        onChange={(val) => form.setFieldValue(field.name, val)}
+                        step={100}
+                        defaultValue={2000}
+                        min={1500}
+                        max={4000}
+                      >
+                        <NumberInputField />
+                        <NumberInputStepper>
+                          <NumberIncrementStepper />
+                          <NumberDecrementStepper />
+                        </NumberInputStepper>
+                      </NumberInput>
+                    </FormControl>
+                  )}
+                </Field>
+                <Button
+                  mt={4}
+                  colorScheme="teal"
+                  isLoading={props.isSubmitting}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+                {/* <pre>{JSON.stringify(props.values, null, 2)}</pre> */}
+              </Stack>
+            </Form>
+          )}
+        </Formik>
       </Box>
     </Center>
   );
