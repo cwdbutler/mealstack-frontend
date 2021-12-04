2d274344667998fdffe6302056f597512bb4a7d8
Douglas <douglaskerins@gmail.com>
2021-12-04 21:44:19 +0000

    adding plan generator

--- src/components/Search/PlanCreator.js
+++ src/components/Search/PlanCreator.js
@@ -1,18 +1,6 @@
 import PlanGeneratorForm from './PlanGeneratorForm';
+import { useState, useEffect } from 'react';
 
 export default function PlanCreator() {
-  const [formData, updateFormData] = useState({});
-
-  const handleChange = (e) => {
-    updateFormData({
-      ...formData,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(formData);
-  };
-
   return <PlanGeneratorForm />;
 }
--- src/components/Search/PlanGeneratorForm.js
+++ src/components/Search/PlanGeneratorForm.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Button } from '@chakra-ui/button';
 import { Box, Center, Stack } from '@chakra-ui/layout';
 import {
@@ -10,6 +11,32 @@ import {
 import { FormControl, FormLabel, FormHelperText } from '@chakra-ui/react';
 
 export default function PlanGeneratorForm() {
+  const initialFormData = {
+    protein: 100,
+    fats: 100,
+    carbs: 100,
+    calories: 2000
+  }
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleChange = (e) => {
+    console.log();
+    setFormData({
+      ...formData,
+    [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+    console.log(e.target); 
+  };
+
   return (
     <Center h="90vh">
       <Box w="50vh">
@@ -17,7 +44,7 @@ export default function PlanGeneratorForm() {
           <Stack spacing={4}>
             <FormControl id="protein">
               <FormLabel>Protein</FormLabel>
-              <NumberInput step={10} defaultValue={100} min={0} max={300}>
+              <NumberInput step={10} defaultValue={formData.protein} min={0} max={300}>
                 <NumberInputField />
                 <NumberInputStepper>
                   <NumberIncrementStepper />
@@ -27,7 +54,7 @@ export default function PlanGeneratorForm() {
             </FormControl>
             <FormControl id="fats">
               <FormLabel>Fats</FormLabel>
-              <NumberInput step={10} defaultValue={100} min={0} max={300}>
+              <NumberInput step={10} defaultValue={formData.fats} min={0} max={300}>
                 <NumberInputField />
                 <NumberInputStepper>
                   <NumberIncrementStepper />
@@ -37,7 +64,7 @@ export default function PlanGeneratorForm() {
             </FormControl>
             <FormControl id="carbs">
               <FormLabel>Carbs</FormLabel>
-              <NumberInput step={10} defaultValue={100} min={0} max={300}>
+              <NumberInput step={10} defaultValue={formData.carbs} min={0} max={300}>
                 <NumberInputField />
                 <NumberInputStepper>
                   <NumberIncrementStepper />
@@ -47,7 +74,7 @@ export default function PlanGeneratorForm() {
             </FormControl>
             <FormControl id="calories">
               <FormLabel>Calories</FormLabel>
-              <NumberInput step={100} defaultValue={2000} min={500} max={8000}>
+              <NumberInput step={100} defaultValue={formData.calories} min={500} max={8000}>
                 <NumberInputField />
                 <NumberInputStepper>
                   <NumberIncrementStepper />
@@ -55,7 +82,7 @@ export default function PlanGeneratorForm() {
                 </NumberInputStepper>
               </NumberInput>
             </FormControl>
-            <Button>Search plans</Button>
+            <Button onClick={handleSubmit}>Search plans</Button>
             <FormHelperText textAlign="center">
               Find your Mealstack
             </FormHelperText>
