9984765b20df6589ef6b644c8922f9e136869283
Douglas <douglaskerins@gmail.com>
2021-12-07 09:40:48 +0000

    fixing PlanInfo merge

--- src/components/PlanInfo/PlanInfo.js
+++ src/components/PlanInfo/PlanInfo.js
@@ -21,60 +21,10 @@ export default function PlanInfo() {
       </Center>
     );
   }
-<<<<<<< HEAD
-};
-
-shoppingListObjectCreator();
-
-const shoppingListDisplay = shoppingListObjectArray.map((item) => (
-  <Box>
-    <Checkbox colorScheme="teal" size="sm">
-      {Math.round(item.weight)}{' '}
-      {Math.round(item.weight) === 1 ? 'gram' : 'grams'} of{' '}
-      {item.name === 'eg' ? 'egg' : item.name}
-    </Checkbox>
-  </Box>
-));
-
-const data = [
-  { title: 'Carbs', value: mockObj.mealplan.carbs, color: '#FED7E2' },
-  { title: 'Fat', value: mockObj.mealplan.fat, color: '#FEEBC8' },
-  { title: 'Protein', value: mockObj.mealplan.protein, color: '#FC8181' },
-];
-
-export default function PlanInfo() {
-  // const { id } = useParams();
-
-
-  return (
-    <>
-      <Center>
-        <Heading mt="4">Plan information</Heading>
-      </Center>
-      <Grid
-        h="80vh"
-        m="4"
-        templateRows="repeat(10, 1fr)"
-        templateColumns="repeat(6, 1fr)"
-        gap={4}
-      >
-        <GridItem
-          rowSpan={4}
-          colSpan={4}
-          bg="teal.50"
-          border="2px"
-          borderColor="gray.200"
-          borderRadius="10"
-        >
-          <Center>
-            <Heading size="md">Nutritional breakdown</Heading>
-          </Center>
-=======
 
   if (!planInfo) {
     return <NotFound />;
   }
->>>>>>> main
 
   return <DetailedPlan plan={planInfo} />;
 }
