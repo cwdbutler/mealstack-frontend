725338225de09a0d33e1c5e499170543153d925b
ConorButler <cwdbutler@gmail.com>
2021-12-07 22:56:39 +0000

    Search error page

--- src/components/Search/SearchIndex.js
+++ src/components/Search/SearchIndex.js
@@ -1,21 +1,49 @@
 import { Center, Box } from '@chakra-ui/layout';
-import { Heading, Wrap, WrapItem } from '@chakra-ui/react';
+import {
+  Heading,
+  Wrap,
+  WrapItem,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react';
 import Plan from './Plan';
 
 function SearchIndex({ plans }) {
   console.log('plans inside SearchIndex', plans);
   if (plans.error) {
     return (
-      <Center mt={5}>
-        <Heading mt={20}>
-          {'Sorry, there seems to be a server error 🙁'}
-        </Heading>
+      <Center h="100%" w="100%">
+        <Alert
+          status="error"
+          variant="subtle"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          width="25%"
+          height="25%"
+          borderRadius="lg"
+        >
+          <AlertIcon boxSize="40px" mr={0} />
+          <AlertTitle mt={4} mb={1} fontSize="xl">
+            Error!
+          </AlertTitle>
+          <AlertDescription fontSize="lg" maxWidth="sm">
+            There appears to be a problem with our server...
+          </AlertDescription>
+        </Alert>
       </Center>
     );
   }
 
   const planList = plans.map((data) => {
-    return <WrapItem><Plan key={data.id.toString()} plan={data} /></WrapItem>;
+    return (
+      <WrapItem>
+        <Plan key={data.id.toString()} plan={data} />
+      </WrapItem>
+    );
   });
 
   return (
